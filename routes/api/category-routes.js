dedcc64881834644a6e41e277fed786788d5a244
jgunn <j.s.gunn22@gmail.com>
2023-09-08 09:26:48 -0700

    creates routes for Category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,39 +29,53 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", (req, res) => {
   // create a new category
-  Category.create({
-    category_name: req.body.category_name,
-  })
-    .then((newCategory) => {
-      res.json(newCategory);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
+  try {
+    Category.create({ category_name: req.body.category_name });
+    res
+      .status(200)
+      .json(`${req.body.category_name} has been added to the Category table.`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
-  Category.update(
-    {
-      category_name: req.body.category_name,
-    },
-    {
-      where: {
-        category_id: req.body.category_id,
+  try {
+    Category.update(
+      {
+        category_name: req.body.category_name,
       },
-    }
-  )
-    .then((updatedCat) => {
-      res.json(updatedCat);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
+      {
+        where: {
+          category_id: req.body.category_id,
+        },
+      }
+    );
+    res
+      .status(200)
+      .json(`Category with ID# ${req.body.category_id} has been updated `);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  try {
+    Category.destroy({
+      where: {
+        category_id: req.body.category_id,
+      },
+    });
+    res
+      .status(200)
+      .json(
+        `Category with ID# ${req.body.category_id} has been deleted from the database.`
+      );
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
